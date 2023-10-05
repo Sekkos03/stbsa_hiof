ff84eeb88e4284485054004a74fdb446c9a52494
borat <berat-murseli@hotmail.com>
2023-10-05 15:42:54 +0200

    oppdatert database og endret på allempoyee metoden

--- software/src/main/java/com/example/software/controller/HovedsideController.java
+++ software/src/main/java/com/example/software/controller/HovedsideController.java
@@ -3,8 +3,12 @@ package com.example.software.controller;
 import com.example.software.Hovedside;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @RestController
 public class HovedsideController {
@@ -22,11 +26,9 @@ public class HovedsideController {
                 "DELETE FROM EMPLOYEE WHERE ID =?", id);
     }
 
-    public Integer allEmpoyee() {
-        return jdbcTemplate.queryForObject(
-                "SELECT id FROM EMPLOYEE", Integer.class);
+    public String allEmpoyee() {
+        return jdbcTemplate.queryForObject("SELECT * FROM EMPLOYEE where ID = 2", (rs, rowNum) -> rs.getInt("ID") + " " + rs.getString("FirstName"));
     }
-
     @GetMapping("/hovedside")
     public Hovedside hovedside() {
         allEmpoyee();
