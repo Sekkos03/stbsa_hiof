6279edfc06d919763023ee14833cfdb88a5ab369
borat <berat-murseli@hotmail.com>
2023-10-05 12:22:24 +0200

    f

--- software/src/main/java/com/example/software/controller/HovedsideController.java
+++ software/src/main/java/com/example/software/controller/HovedsideController.java
@@ -1,6 +1,7 @@
 package com.example.software.controller;
 
 import com.example.software.Hovedside;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
