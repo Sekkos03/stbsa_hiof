28aba9ca1894e8b7fd9b325d7e9d9908913d6e4c
borat <berat-murseli@hotmail.com>
2023-10-05 12:32:51 +0200

    lagt til EMPLOYEE tabel og lagt til addEmplyee i controller

--- software/src/main/java/com/example/software/controller/HovedsideController.java
+++ software/src/main/java/com/example/software/controller/HovedsideController.java
@@ -8,6 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HovedsideController {
 
+    JdbcTemplate jdbcTemplate = new JdbcTemplate();
+
+    public int addEmplyee(int id) {
+        return jdbcTemplate.update(
+                "INSERT INTO EMPLOYEE VALUES (?)", id);
+    }
+
     @GetMapping("/hovedside")
     public Hovedside hovedside() {
      return new Hovedside();
