5a76447d968b90d8637ebe4efe5f9c25c15ad644
borat <berat-murseli@hotmail.com>
2023-10-09 13:53:55 +0200

    lagde database for software prosjektet

--- software/src/main/java/com/example/software/Hovedside.java
+++ software/src/main/java/com/example/software/Hovedside.java
@@ -1,4 +1,4 @@
 package com.example.software;
 
-public record Hovedside() {
+public class Hovedside {
 }
--- software/src/main/java/com/example/software/controller/HovedsideController.java
+++ software/src/main/java/com/example/software/controller/HovedsideController.java
@@ -7,31 +7,16 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 @RestController
 public class HovedsideController {
 
     @Autowired
     JdbcTemplate jdbcTemplate = new JdbcTemplate();
 
-    public int addEmplyee(int id, String firstName) {
-        return jdbcTemplate.update(
-                "INSERT INTO EMPLOYEE VALUES (?, ?)", id, firstName);
-    }
 
-    public int removeEmplyee(int id) {
-        return jdbcTemplate.update(
-                "DELETE FROM EMPLOYEE WHERE ID =?", id);
-    }
 
-    public String allEmpoyee() {
-        return jdbcTemplate.queryForObject("SELECT * FROM EMPLOYEE where ID = 2", (rs, rowNum) -> rs.getInt("ID") + " " + rs.getString("FirstName"));
-    }
     @GetMapping("/hovedside")
-    public Hovedside hovedside() {
-        allEmpoyee();
-        return new Hovedside();
+    public String hovedside() {
+        return null;
     }
 }
