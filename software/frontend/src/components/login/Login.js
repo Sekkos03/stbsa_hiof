f3d40e875bba82939b6f522002d6f7ccad9a5ae8
borat <berat-murseli@hotmail.com>
2023-10-18 13:11:19 +0200

    knappene for å logge inn er mer dynamsik ved bruk av Link istedenfor <a>

--- software/frontend/src/components/login/Login.js
+++ software/frontend/src/components/login/Login.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 function Login() {
     return (
         <div className="container h-100 text-center" >
             <h1 className="h3 mb-3 font-weight-normal">Login</h1>
             <div>
-                <a href="http://localhost:3000/guideMainPage" className={"px-1 text-white"}><button className="btn btn-primary btn-lg"> Login as guide</button></a>
-                <a href="http://localhost:3000/guideMainPage" className={"px-1 text-white"}><button className="btn btn-primary btn-lg">login as user</button></a>
+                <Link to="/GuideMainPage" className={"px-1 text-white"}>
+                <button className="btn btn-primary btn-lg"> Login as guide</button>
+                </Link>
+                <Link to="/UserMainPage" className={"px-1 text-white"}>
+                <button className="btn btn-primary btn-lg">login as user</button>
+                </Link>
             </div>
         </div>
     );
-};
+}
 
 export default Login;
