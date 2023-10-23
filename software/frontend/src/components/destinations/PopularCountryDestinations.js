4c33a36e5a183625e556f3ea8cbdd1c331e6a035
borat <berat-murseli@hotmail.com>
2023-10-23 12:45:34 +0200

    henter nå data fra databsen og viser det. lagt il link sån at hver card kan sende deg til siden for det du tryker på

--- software/frontend/src/components/destinations/PopularCountryDestinations.js
+++ software/frontend/src/components/destinations/PopularCountryDestinations.js
@@ -1,29 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Card, Col, Row} from "react-bootstrap";
 import picture1 from "../../pictures/picture1.jpg";
 import '../../pages/MainPage.css';
+import {Link} from "react-router-dom";
 
 
 function MainPageCountryDestination() {
 
+    const [countries, setCountry] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/allcountry')
+            .then(response => response.json())
+            .then(data => setCountry(data))
+            .catch(error => console.error(error));
+    }, []);
+
     return(
-    <div className="container" style={{marginBottom: "5vh"}}>
-        <div className = "h1" style={{ marginBottom: '5vh' }}>Popular Tour Countries</div>
-        <Row xs={1} md={4} className="g-3">
-            {Array.from({ length: 8 }).map((_, idx) => <Col key={idx}>
-                <Card>
-                    <Card.Img variant="top" src = {picture1} />
-                    <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            This is a longer card with supporting text below as a natural
-                            lead-in to additional content. This content is a little bit
-                            longer.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            </Col>)}
+    <div className="container" style={{ marginBottom: '5px' }}>
+        <div className="h1" style={{ marginBottom: '5vh' }}>Popular Tour Country</div>
+        <Row className="g-3">
+            {countries.slice(0, 8).map((country) => (
+                <Col key={country.countryID} xs={12} sm={6} md={4} lg={3}>
+                    <Link to={"/" + country.countryID} style={{textDecoration: "none" }}>
+                    <Card>
+                        <Card.Img variant="top" src={country.country_picture} />
+                        <Card.Body>
+                            <Card.Title>{country.country}</Card.Title>
+                        </Card.Body>
+                    </Card>
+                    </Link>
+                </Col>
+            ))}
         </Row>
     </div>
     )
