cb3c047eda8a5cde5812b83ceb4dad17cfb35fdd
argoarin <arin1999@hotmail.com>
2023-10-25 13:25:29 +0200

    Kalender

--- software/frontend/src/pages/GuideMainPage.js
+++ software/frontend/src/pages/GuideMainPage.js
@@ -1,11 +1,21 @@
-import React from 'react'
-import HeaderGuide from "../components/header/HeaderGuide";
+import React, {useState} from 'react';
+import Calendar from 'react-calendar';
 
 function GuideMainPage(){
+    const [date, setDate] = useState(new Date());
     return(
         <div>
-            <HeaderGuide />
             <h1>Guide Main Page</h1>
+            <div className='calender'>
+                <h1 className='text-center'>React Calendar</h1>
+                <div className='calendar-container'>
+                    <Calendar onChange={setDate} value={date} />
+                </div>
+                <p className='text-center'>
+                    <span className='bold'>Selected Date:</span>{' '}
+                    {date.toDateString()}
+                </p>
+            </div>
         </div>
     )
 }
