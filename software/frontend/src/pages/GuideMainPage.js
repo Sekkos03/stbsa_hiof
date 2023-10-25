f88ec719ffeb0e6a4e9f905324679a184a9023ae
borat <berat-murseli@hotmail.com>
2023-10-25 10:15:22 +0200

    kopierte kode fra Header til en ny header for guide.

--- software/frontend/src/pages/GuideMainPage.js
+++ software/frontend/src/pages/GuideMainPage.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import HeaderGuide from "../components/header/HeaderGuide";
 
 function GuideMainPage(){
     return(
         <div>
+            <HeaderGuide />
             <h1>Guide Main Page</h1>
         </div>
     )
