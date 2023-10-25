4223c58ce2652c640345cea21437aa9a6a66a6bc
borat <berat-murseli@hotmail.com>
2023-10-25 10:11:52 +0200

    lagt til route for guide sin hovedside

--- software/frontend/src/App.js
+++ software/frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/login/Login";
 import MainPage from "./pages/MainPage";
+import GuideMainPage from "./pages/GuideMainPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
                 <Route path="/" element={<MainPage/>}/>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/guide" element={<Login/>}/>
+                <Route path="/GuideMainPage" element={<GuideMainPage/>}/>
             </Routes>
         </BrowserRouter>
     </div>
