aff8a09b340834424063c8b3c30fbee7c378a728
borat <berat-murseli@hotmail.com>
2023-10-28 17:01:30 +0200

    bruker axios til å hente data, sender informasjon om bruker til Header. viser nå også titel. bokkingForm får nå informasjon om touristID og tourID

--- software/frontend/src/pages/ToursPage.js
+++ software/frontend/src/pages/ToursPage.js
@@ -1,24 +1,42 @@
 import React, {useEffect, useState} from 'react'
-import Header from "../components/header/Header";
 import {useParams} from "react-router-dom";
 import picture from "./picture1.jpg"
-import BokingForm from "../components/BokingForm";
+import Header from "../components/header/Header";
+import BookingForm from "../components/BookingForm";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios'
 
 function ToursPage(){
 
     const [tour, setTour] = useState({});
     const { tourID } = useParams();
+    const { UserID } = useParams();
+    const [user, setUser] = useState({})
 
     useEffect(() => {
-        fetch('http://localhost:8080/OneTour/' + tourID)
-            .then(response => response.json())
-            .then(data => setTour(data))
-            .catch(error => console.error(error));
-    });
+        axios.get(`http://localhost:8080/OneTour/${tourID}`)
+            .then(response => {
+                setTour(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            },);
+
+        axios.get('http://localhost:8080/tourist/' + UserID)
+            .then(response => {
+                setUser(response.data);
+                console.log(UserID)
+            })
+            .catch(error => {
+                console.error(error);
+                console.log(UserID)
+            });
+    }, [UserID, tourID]);
 
     return(
         <>
-            <Header />
+            <Header user={user}/>
+
             <div className="container mt-4">
                 <div className="row">
                     <div className="col-md-6">
@@ -29,13 +47,14 @@ function ToursPage(){
                         />
                     </div>
                     <div className="col-md-6">
-                        <h1 className="mb-3">{tour.name}</h1>
+                        <h1 className="mb-3">{tour.title}</h1>
                         <p className="lead">{tour.description}</p>
                         <p className="text-muted">Price: ${tour.price}</p>
                     </div>
                 </div>
             </div>
-            <BokingForm maxPeople={tour.maxPeople}/>
+
+            <BookingForm maxPeople={tour.maxPeople} touristID={UserID} tourID={tourID}/>
         </>
     )
 }
