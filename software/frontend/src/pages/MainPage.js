fe97630a0d34ebc053445b677dd7c0bd933bdfb6
borat <berat-murseli@hotmail.com>
2023-10-28 16:39:36 +0200

    main page henter informasjon om hvem som er logget inn og sender det videre til Header

--- software/frontend/src/pages/MainPage.js
+++ software/frontend/src/pages/MainPage.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from '../components/header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import picture1 from "../pictures/picture1.jpg";
 import './MainPage.css'
-import MainPageCountryDestination from "../components/destinations/PopularCountryDestinations";
+import PopulareCountryDestination from "../components/destinations/PopularCountryDestinations";
 import MainPageCityDestination from "../components/destinations/populareCitydestination";
 import PopularTour from "../components/PopularTour";
+import {useParams} from "react-router-dom";
 
 
 function MainPage() {
 
+    const [tourist, setTourist] = useState({});
+    const { UserID } = useParams();
+
+    useEffect(() => {
+        if (UserID) {
+            fetch('http://localhost:8080/tourist/' + UserID)
+                .then(response => response.json())
+                .then(data => setTourist(data))
+                .catch(error => console.error(error));
+        }
+        else {
+            setTourist(null)
+        }
+    }, [UserID]);
+
     return (
         <>
-            <Header />
+            <Header user={tourist}/>
             <div className = "image-Container">
-            <img src = {picture1} id = "big-Picture" className=''/>
+            <img src = {"/pictures/picture1.jpg"} id = "big-Picture" className=''/>
             </div>
-            <MainPageCountryDestination/>
+            <PopulareCountryDestination/>
             <MainPageCityDestination/>
             <PopularTour />
+
+
         </>
     );
 }
