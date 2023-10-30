4478c78ed6fa8cebdcf7d6fe718e12c743493c45
argoarin <arin1999@hotmail.com>
2023-10-30 13:26:10 +0100

    Kalender

---
+++ software/frontend/src/pages/OpprettGuidePage.js
@@ -0,0 +1,20 @@
+import React, {useEffect, useState} from 'react';
+import Header from '../components/header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './MainPage.css'
+import PopulareCountryDestination from "../components/destinations/PopularCountryDestinations";
+import MainPageCityDestination from "../components/destinations/populareCitydestination";
+import PopularTour from "../components/PopularTour";
+import {useParams} from "react-router-dom";
+
+
+function OpprettGuidePage() {
+
+    return (
+        <>
+        </>
+    );
+}
+
+export default OpprettGuidePage;
+
