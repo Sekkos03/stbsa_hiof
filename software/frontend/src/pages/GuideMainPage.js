db1472176333b460670ae73a34f4c1ded13232d0
argoarin <arin1999@hotmail.com>
2023-10-30 12:21:35 +0100

    Kalender

--- software/frontend/src/pages/GuideMainPage.js
+++ software/frontend/src/pages/GuideMainPage.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import './GuideMainPage.css'
 function GuideMainPage(){
 
     const [date, setDate] = useState(new Date());
