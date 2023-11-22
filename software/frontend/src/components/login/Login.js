6bd8ac5d22a21990f15c3ba130b90f7a67f12f1f
berat <Berat-murseli@hotmail.com>
2023-11-22 01:25:53 +0100

    knappen for å logge inn som guide er fikset så nå sender den til riktig link

--- software/frontend/src/components/login/Login.js
+++ software/frontend/src/components/login/Login.js
@@ -7,7 +7,7 @@ function Login() {
         <div className="container h-100 text-center" >
             <h1 className="h3 mb-3 font-weight-normal">Login</h1>
             <div>
-                <Link to="/GuideMainPage" className={"px-1 text-white"}>
+                <Link to="/Guide/1/GuideMainPage" className={"px-1 text-white"}>
                 <button className="btn btn-primary btn-lg"> Login as guide</button>
                 </Link>
                 <Link to="/user/1" className={"px-1 text-white"}>
