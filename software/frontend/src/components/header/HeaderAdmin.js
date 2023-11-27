c81a3ca62cc20381355776bb20724b830ecaf8d9
berat <Berat-murseli@hotmail.com>
2023-11-27 21:42:42 +0100

    endret farge på header tekst

--- software/frontend/src/components/header/HeaderAdmin.js
+++ software/frontend/src/components/header/HeaderAdmin.js
@@ -14,7 +14,7 @@ function HeaderAdmin() {
                     </a>
 
                     <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                        <li><a href="#" className="nav-link px-2 text-secondary">Home</a></li>
+                        <li><a href="#" className="nav-link px-2 text-white">Home</a></li>
                         <li><a href="#" className="nav-link px-2 text-white">Features</a></li>
                     </ul>
 
