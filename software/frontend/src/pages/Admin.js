1bb2b2714684a908fd3eec0bdf7934fc1696c456
berat <Berat-murseli@hotmail.com>
2023-11-27 16:31:48 +0100

    lagde en side for admin som viser alle tours og admin har muligheten til å slette dem

--- software/frontend/src/pages/Admin.js
+++ software/frontend/src/pages/Admin.js
@@ -1,7 +1,39 @@
-function admin() {
+import React, {useEffect, useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HeaderAdmin from "../components/header/HeaderAdmin";
+import Tour from "../components/Tour";
+import axios from "axios";
+
+function Admin() {
+
+    const [BookedTours, setBookedTours] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/getAllBookdeTours")
+            .then(response => {
+                setBookedTours(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+        }, []);
+
+
+
     return (
-        <div>
-            <h1>Admin</h1>
-        </div>
+        <>
+            <HeaderAdmin/>
+            <div className="container">
+                <h1>Admin</h1>
+                <div className="h1" style={{ marginBottom: '5vh' }}>All Booked Tours</div>
+                <div className="row">
+                    {BookedTours.map((BookedTour) => (
+                        <Tour key={BookedTour.bookedTourID} bookedTour={BookedTour} />
+                    ))}
+                </div>
+            </div>
+        </>
     )
 }
+
+export default Admin;
